8ba3c16acb346186c04dbcbeabf5171873f78fc8
Ludmila Lee <ludlee@yandex.ru>
2017-06-30 17:32:59 +0300

    Created packages

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import time
+
 import pytest
-from contact import Contact
-from application import Application
+
+from fixture.application import Application
+from model.contact import Contact
 
 
 @pytest.fixture
