8df537fb973d0fcd6b00a9ebf4855f16c363a759
Ludmila Lee <ludlee@yandex.ru>
2017-07-06 19:16:38 +0300

    Added del_group and common fixture for session

---
+++ test/test_del_group.py
@@ -0,0 +1,8 @@
+#from model.group import Group
+
+def test_delete_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.open_groups_page()
+    app.group.delete_first_group()
+    app.group.open_groups_page()
+    app.session.logout()
