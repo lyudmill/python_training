b43814ee1af9b02f8efd1cc0254b6e80c1bb7282
Ludmila Lee <ludlee@yandex.ru>
2017-07-07 20:54:38 +0300

    Added tests for delete and edit contact

---
+++ test/test_del_contact.py
@@ -0,0 +1,9 @@
+import time
+
+
+def test_delete_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.delete_first_contact()
+    app.open_home_page()
+    time.sleep(3)
+    app.session.logout()
---
+++ test/test_edit_contact.py
@@ -0,0 +1,15 @@
+import time
+from model.contact import Contact
+
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(firstname="Sergey", midname="S.", lastname="Sergeev", nickname="Serg",
+                       title="Manager", company="Big_company_pro", street="Lenina Street,77",
+                       homephone="+7(812)77777", mobilephone="+7(911)77777", workphone="+7(812)3333337",
+                       email="sergey.sergeev.big_company.com", email2="iva.ivanov@big_company_pro.com",
+                       byear="1991", homepage="www.sergeev.com", address2="new_adr",
+                       notes="Some comments"))
+    app.open_home_page()
+    time.sleep(3)
+    app.session.logout()
