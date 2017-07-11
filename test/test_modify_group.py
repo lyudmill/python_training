43173d91c09f751e83fae978fa7a594e844932bf
Ludmila Lee <ludlee@yandex.ru>
2017-07-11 19:53:01 +0300

    Added group modification test

---
+++ test/test_modify_group.py
@@ -0,0 +1,15 @@
+import time
+from model.group import Group
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="Modified group"))
+    time.sleep(3)
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="modified header"))
+    time.sleep(3)
+    app.session.logout()
