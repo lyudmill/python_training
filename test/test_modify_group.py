629010a5542eaf924d84a96ee95b2050b0e3114b
Ludmila Lee <ludlee@yandex.ru>
2017-07-12 16:08:47 +0300

    Add fixture validation, move login and logout to fixture level

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,13 +3,10 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="Modified group"))
     time.sleep(3)
-    app.session.logout()
+
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="modified header"))
     time.sleep(3)
-    app.session.logout()
