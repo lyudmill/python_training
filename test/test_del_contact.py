7c4c2a1afc02efe72ad495ec24d90cf589b03f3f
Ludmila Lee <ludlee@yandex.ru>
2017-07-26 21:24:43 +0300

    plus to previous

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -10,8 +10,6 @@ def test_delete_some_contact(app):
     old_list = app.contact.get_contacts_list()
     index = randrange(len(old_list))
     app.contact.delete_contact_by_index(index)
-    app.open_home_page()
-    time.sleep(10)
     assert len(old_list)-1 == app.contact.count()
     new_list = app.contact.get_contacts_list()
     old_list[index:index + 1] = []
@@ -22,11 +20,10 @@ def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.open_contact_page()
         app.contact.create(Contact())
-        time.sleep(5)
     old_list = app.contact.get_contacts_list()
+    print(old_list)
     app.contact.delete_first_contact()
-    app.open_home_page()
-    time.sleep(10)
+    print("Contact count: %s" % app.contact.count())
     assert len(old_list)-1 == app.contact.count()
     new_list=app.contact.get_contacts_list()
     old_list[0:1]=[]
