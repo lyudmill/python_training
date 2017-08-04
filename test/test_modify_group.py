4191271ea230cc570657419a25e6645bcd934b28
Ludmila Lee <ludlee@yandex.ru>
2017-08-04 12:53:17 +0300

    Added DDT in group tests

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,13 +1,25 @@
 from model.group import Group
-from random import randrange
+import random
+import pytest
+import string
 
 
-def test_modify_group_name(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Group(name=random_string("mod_name", 7), header=random_string("mod_header", 15), footer=random_string("mod_footer", 12))
+            for i in range(3)
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+
+
+def test_modify_group(app, group):
     if app.group.count() == 0:
         app.group.create(Group(name="New group"))
     old_groups = app.group.get_group_list()
-    group = Group(name="Modified group")
-    index = randrange(len(old_groups))
+    index = random.randrange(len(old_groups))
     group.id = old_groups[index].id
     app.group.modify_group_by_index(index, group)
     assert len(old_groups) == app.group.count()
@@ -15,14 +27,3 @@ def test_modify_group_name(app):
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="New_group"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name = old_groups[index].name, gr_id = old_groups[index].id, header="Modified header")
-    app.group.modify_group_by_index(index, group)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
