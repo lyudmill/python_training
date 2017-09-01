e73ca1f505f97ff0dd50ce608aafcba92d0662e2
Ludmila Lee <ludlee@yandex.ru>
2017-09-01 04:15:33 +0300

    Add allure steps

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+import pytest
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups,key=Group.id_or_max) ==sorted(new_groups,key=Group.id_or_max)
-    if check_ui:
-        assert len(old_groups) == app.group.count()
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
-        print("***UI OK**")
+    with pytest.allure.step("Given a group list"):
+        old_groups = db.get_group_list()
+    with pytest.allure.step("When I add the group to the list"):
+        app.group.create(group)
+    with pytest.allure.step("Then the new group list is equal to the old list with the added group"):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups,key=Group.id_or_max) ==sorted(new_groups,key=Group.id_or_max)
+        if check_ui:
+            assert len(old_groups) == app.group.count()
+            assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+            print("***UI OK**")
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,17 +1,21 @@
 from model.group import Group
 import random
-
+import pytest
 
 def test_delete_some_group(app, db, check_ui):
-    if len(db.get_group_list()) == 0:
-        app.group.create(Group(name="name for delete"))
-        app.group.return_to_groups_page()
-    old_groups = db.get_group_list()
-    group = random.choice(old_groups)
-    app.group.delete_group_by_id(group.id)
-    new_groups = db.get_group_list()
-    assert len(old_groups) -1 == len(new_groups)
-    old_groups.remove(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with pytest.allure.step("Given a non-empty group list"):
+        if len(db.get_group_list()) == 0:
+            app.group.create(Group(name="name for delete"))
+            app.group.return_to_groups_page()
+    with pytest.allure.step("Given a random group from the list"):
+        old_groups = db.get_group_list()
+        group = random.choice(old_groups)
+    with pytest.allure.step("When I delete the group from the list"):
+        app.group.delete_group_by_id(group.id)
+    with pytest.allure.step("Then the new group list is equal to the old list without the deleted group"):
+        new_groups = db.get_group_list()
+        assert len(old_groups) -1 == len(new_groups)
+        old_groups.remove(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
